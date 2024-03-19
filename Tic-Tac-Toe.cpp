6a19319e99514d7a65fed70ef1f0c2b8efe460d3
arcmrx <arturvolkov44@gmail.com>
2024-03-19 21:37:08 +0300

    Update

--- Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe.cpp
@@ -3,6 +3,10 @@
 
 void print_array(std::vector<std::vector<char>> Field)
 {
+    std::cout << "\x1B[2J\x1B[H";
+    std::cout << "-----------------\n";
+    std::cout << "|Крестики-нолики|\n";
+    std::cout << "-----------------\n";
     std::cout << " 0 | 1 | 2 | 3 |\n";
     std::cout << "–––+–––+–––+–––+\n";
     std::cout << " 1 | " << Field[0][0] << " | " << Field[0][1] << " | " << Field[0][2] << " |\n";
@@ -33,7 +37,9 @@ void moveX(std::vector<std::vector<char>> &Field)
     {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Ошибка! Введите корректные координаты.\n";
+        std::cout << "\x1B[2J\x1B[H";
+        print_array(Field);
+        std::cout << "\x1b[31mОшибка! Введите корректные координаты.\x1b[0m\n";
         std::cout << "Ход X! Введите координаты:\n";
     }
 
@@ -48,7 +54,9 @@ void moveO(std::vector<std::vector<char>> &Field)
     {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Ошибка! Введите корректные координаты.\n";
+        std::cout << "\x1B[2J\x1B[H";
+        print_array(Field);
+        std::cout << "\x1b[31mОшибка! Введите корректные координаты.\x1b[0m\n";
         std::cout << "Ход 0! Введите координаты:\n";
     }
 
@@ -62,15 +70,15 @@ bool checkWin(std::vector<std::vector<char>> &Field)
         if ((Field[i][0] == 'X' && Field[i][0] == Field[i][1] && Field[i][1] == Field[i][2]) ||
             (Field[0][i] == 'X' && Field[0][i] == Field[1][i] && Field[1][i] == Field[2][i]))
         {
-            std::cout << "Игрок X выиграл!\n";
             print_array(Field);
+            std::cout << "\x1b[1;32mИгрок X выиграл!\x1b[0m\n";
             return false;
         }
         else if ((Field[i][0] == '0' && Field[i][0] == Field[i][1] && Field[i][1] == Field[i][2]) ||
                  (Field[0][i] == '0' && Field[0][i] == Field[1][i] && Field[1][i] == Field[2][i]))
         {
-            std::cout << "Игрок 0 выиграл!\n";
             print_array(Field);
+            std::cout << "\x1b[1;32mИгрок 0 выиграл!\x1b[0m\n";
             return false;
         }
     }
@@ -78,15 +86,15 @@ bool checkWin(std::vector<std::vector<char>> &Field)
     if ((Field[0][0] == 'X' && Field[1][1] == 'X' && Field[2][2] == 'X') ||
         (Field[2][0] == 'X' && Field[1][1] == 'X' && Field[0][2] == 'X'))
     {
-        std::cout << "Игрок X выиграл!\n";
         print_array(Field);
+        std::cout << "\x1b[1;32mИгрок X выиграл!\x1b[0m\n";
         return false;
     }
     else if ((Field[0][0] == '0' && Field[1][1] == '0' && Field[2][2] == '0') ||
              (Field[2][0] == '0' && Field[1][1] == '0' && Field[0][2] == '0'))
     {
-        std::cout << "Игрок 0 выиграл!\n";
         print_array(Field);
+        std::cout << "\x1b[1;32mИгрок 0 выиграл!\x1b[0m\n";
         return false;
     }
 
@@ -108,8 +116,8 @@ bool checkWin(std::vector<std::vector<char>> &Field)
     }
     if (isDraw)
     {
-        std::cout << "Ничья!\n";
         print_array(Field);
+        std::cout << "\x1b[1;32mНичья!\x1b[0m\n";
         return false;
     }
 
@@ -119,11 +127,6 @@ bool checkWin(std::vector<std::vector<char>> &Field)
 int main()
 {
     setlocale(LC_ALL, "");
-    std::cout << "\x1B[2J\x1B[H";
-    std::cout << "\n-----------------\n";
-    std::cout << "|Крестики-нолики|\n";
-    std::cout << "-----------------\n";
-
     std::vector<std::vector<char>> Field(3, std::vector<char>(3));
     for (int i = 0; i != 3; i++)
     {
@@ -138,12 +141,14 @@ int main()
     while (checkWin(Field))
     {
         moveX(Field);
+        std::cout << "\x1B[2J\x1B[H";
         if (!checkWin(Field))
         {
             break;
         }
         print_array(Field);
         moveO(Field);
+        std::cout << "\x1B[2J\x1B[H";
         if (!checkWin(Field))
         {
             break;
